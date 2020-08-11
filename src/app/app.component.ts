49f732252cb7746d44889260bf191bc0681aef05
LuisMayo <luismayovalbuena@outlook.es>
2020-08-12 00:05:38 +0200

    Initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'coordinate-viewer';
+  width: string;
+  height: string;
+  coordinates: string;
+  coordinateMap = new Map<string, string[]>();
+  displayType = 'rowFirst';
+
+  changeCoordinates(): void {
+    const coordinates = this.coordinates.split(';');
+    for (const coordinate of coordinates) {
+      let [x, y, z] = coordinate.split(',');
+      if (this.displayType === 'colFirst') {
+        [x, y, z] = [y, x, z];
+      }
+      this.coordinateMap.get(x + ',' + y).push(z);
+    }
+  }
+
+  initMap(): void {
+    this.coordinateMap.clear();
+    for (let i = 0; i < +this.height; i++) {
+      for (let j = 0; j < +this.width; j++) {
+        this.coordinateMap.set(i + ',' + j, []);
+      }
+    }
+    this.changeCoordinates();
+  }
 }
